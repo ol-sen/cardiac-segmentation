8d6db41563aaae847d099491784537932f45d657
ol-sen <olga.senyukova@graphics.cs.msu.ru>
2019-06-18 15:38:57 +0300

    MultiGPUModelCheckpoint was fixed: saving is possible.
    Now it is possible to automatically save the model after each epoch when the monitored quantity improves.

--- scripts/train.py
+++ scripts/train.py
@@ -7,6 +7,7 @@ import argparse
 import logging
 import matplotlib.pyplot as plt
 import tensorflow as tf
+import numpy as np
 
 from keras import losses, optimizers, utils
 from keras.optimizers import SGD, RMSprop, Adagrad, Adadelta, Adam, Adamax, Nadam
@@ -55,14 +56,75 @@ def save_plot(figname, history):
     plt.show()
     plt.savefig(figname, bbox_inches='tight')
 
-class MyModelCheckpoint(callbacks.Callback):
-    def __init__(self, model, filepath, monitor, verbose, save_best_only, mode):
-         self.model_to_save = model
-         self.filepath = filepath
-         self.monitor = monitor
-         self.verbose = verbose
-         self.save_best_only = save_best_only
-         self.mode = mode
+class MultiGPUModelCheckpoint(callbacks.Callback):
+
+    def __init__(self, filepath, base_model, monitor='val_loss', verbose=0,
+                 save_best_only=False, save_weights_only=False,
+                 mode='auto', period=1):
+        super(MultiGPUModelCheckpoint, self).__init__()
+        self.base_model = base_model
+        self.monitor = monitor
+        self.verbose = verbose
+        self.filepath = filepath
+        self.save_best_only = save_best_only
+        self.save_weights_only = save_weights_only
+        self.period = period
+        self.epochs_since_last_save = 0
+
+        if mode not in ['auto', 'min', 'max']:
+            warnings.warn('ModelCheckpoint mode %s is unknown, '
+                          'fallback to auto mode.' % (mode),
+                          RuntimeWarning)
+            mode = 'auto'
+
+        if mode == 'min':
+            self.monitor_op = np.less
+            self.best = np.Inf
+        elif mode == 'max':
+            self.monitor_op = np.greater
+            self.best = -np.Inf
+        else:
+            if 'acc' in self.monitor or self.monitor.startswith('fmeasure'):
+                self.monitor_op = np.greater
+                self.best = -np.Inf
+            else:
+                self.monitor_op = np.less
+                self.best = np.Inf
+
+    def on_epoch_end(self, epoch, logs=None):
+        logs = logs or {}
+        self.epochs_since_last_save += 1
+        if self.epochs_since_last_save >= self.period:
+            self.epochs_since_last_save = 0
+            filepath = self.filepath.format(epoch=epoch + 1, **logs)
+            if self.save_best_only:
+                current = logs.get(self.monitor)
+                if current is None:
+                    warnings.warn('Can save best model only with %s available, '
+                                  'skipping.' % (self.monitor), RuntimeWarning)
+                else:
+                    if self.monitor_op(current, self.best):
+                        if self.verbose > 0:
+                            print('Epoch %05d: %s improved from %0.5f to %0.5f,'
+                                  ' saving model to %s'
+                                  % (epoch + 1, self.monitor, self.best,
+                                     current, filepath))
+                        self.best = current
+                        if self.save_weights_only:
+                            self.base_model.save_weights(filepath, overwrite=True)
+                        else:
+                            self.base_model.save(filepath, overwrite=True)
+                    else:
+                        if self.verbose > 0:
+                            print('Epoch %05d: %s did not improve' %
+                                  (epoch + 1, self.monitor))
+            else:
+                if self.verbose > 0:
+                    print('Epoch %05d: saving model to %s' % (epoch + 1, filepath))
+                if self.save_weights_only:
+                    self.base_model.save_weights(filepath, overwrite=True)
+                else:
+                    self.base_model.save(filepath, overwrite=True)
 
 def train():
     logging.basicConfig(level=logging.INFO)
@@ -187,8 +249,8 @@ def train():
                 args.outdir, "weights-{epoch:02d}-{val_dice:.4f}.hdf5") 
         
         if args.multi_gpu:
-            checkpoint = MyModelCheckpoint(m,
-                filepath, monitor=monitor, verbose=1,
+            checkpoint = MultiGPUModelCheckpoint(
+                filepath, m, monitor=monitor, verbose=1,
                 save_best_only=True, mode=mode)
         else:
             checkpoint = ModelCheckpoint(
